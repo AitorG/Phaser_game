5528ffc0bbbe6607685dcabbf03195defe00a566
AitorG <tormentilla92@gmail.com>
2023-01-20 16:57:48 +0100

    delete all game to start from the begining

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,10 +1,7 @@
-import Snake from './Snake'
-import Food from './Food'
 
 export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: 'Game' })
-    this.board = []
   }
 
   create() {
@@ -13,58 +10,6 @@ export default class Game extends Phaser.Scene {
       font: '20px Arial',
     })
 
-    // matriz de 18x24
-    for (let i = 0; i < 24; i++) {
-      this.board[i] = []
-      for (let x = 0; x < 18; x++) {
-        this.board[i][x] = 0
-      }
-    }
-    console.log(this.board)
-
-    this.snake = new Snake(this, 5, 2, 'square')
-
-    this.food = new Food(this)
-
-    // Teclado alfabético
-    this.up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W) // ArrowUp
-    this.down = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S) // ArrowDown
-    this.left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A) // ArrowLeft
-    this.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D) // ArrowRight
-
-    // Flechas de dirección
-    this.arrowUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP) // ArrowUp
-    this.arrowDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN) // ArrowDown
-    this.arrowLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT) // ArrowLeft
-    this.arrowRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT) // ArrowRight
-
-    this.up.onDown = () => {
-      this.snake.setDirection(1)
-    }
-    this.down.onDown = () => {
-      this.snake.setDirection(3)
-    }
-    this.left.onDown = () => {
-      this.snake.setDirection(4)
-    }
-    this.right.onDown = () => {
-      this.snake.setDirection(2)
-    }
-
-    this.arrowUp.onDown = () => {
-      this.snake.setDirection(1)
-    }
-    this.arrowDown.onDown = () => {
-      this.snake.setDirection(3)
-    }
-    this.arrowLeft.onDown = () => {
-      this.snake.setDirection(4)
-    }
-    this.arrowRight.onDown = () => {
-      this.snake.setDirection(2)
-    }
-    
-    
   }
 
   update() {}
--- src/preload/Preload.js
+++ src/preload/Preload.js
@@ -4,22 +4,7 @@ export default class Preload extends Phaser.Scene {
   }
 
   preload() {
-    const progressBar = this.add.graphics()
-    const width = this.cameras.main.width
-
-    this.load.on("progress", (value) => {
-      progressBar.clear()
-      progressBar.fillStyle(0xee7722, 1)
-      progressBar.fillRect(width / 2 - 320 / 2 + 10, 310, 300 * value, 30)
-    })
-
-    this.load.on("complete", () => {
-      progressBar.destroy()
-    })
-
-    //Load Assets
-    this.load.image("square", "./assets/square.png")
-    this.load.image("food", "./assets/food.png")
+   
   }
 
   create() {
