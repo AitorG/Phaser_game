cfcd39c47edeac1602fca26a81e8752341dfa699
AitorG <tormentilla92@gmail.com>
2023-01-25 18:48:27 +0100

    fixed collide

--- src/game/Game.js
+++ src/game/Game.js
@@ -14,20 +14,17 @@ export default class Game extends Phaser.Scene {
       color: '#FFFFFF',
       font: '20px Arial',
     })
-    const image = this.add.image(50, 100, "cuadradoNaranja")
-    const image1 = this.add.image(100, 100, "cuadradoVerde")
-    this.physics.add.existing(image)
-    this.physics.add.existing(image1)
-    this.physics.arcade.collide(this.image, this.image1)
-    image.body.setCollideWorldBounds()
-    image1.body.setCollideWorldBounds()
-    this.matter.world.enable(image, image1)
-    image.body.setDensity(30)
-    image1.body.setDensity(30)
-    image.body.setBounce(30)
-    image1.body.setBounce(30)
-    image.body.setVelocityX(30)
-    image1.body.setVelocityX(30)
+    this.image = this.add.image(50, 100, "cuadradoNaranja")
+    this.image1 = this.add.image(100, 100, "cuadradoVerde")
+    this.physics.add.existing(this.image)
+    this.physics.add.existing(this.image1)
+    this.image.body.setCollideWorldBounds()
+    this.image1.body.setCollideWorldBounds()
+    this.image.body.setBounce(1)
+    this.image1.body.setBounce(1)
+    this.image.body.setVelocity(200)
+    this.image1.body.setVelocity(140)
+    this.physics.add.collider(this.image, this.image1)
   }
 
   update() {}
--- src/index.js
+++ src/index.js
@@ -19,11 +19,7 @@ const config = {
   ],
   physics: {
     default: 'arcade',
-    arcade: {
-      gravity: {
-        y: 110
-      }
-    }
+    arcade: {}
   }
 }
 
