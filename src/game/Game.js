1c93f7e3a19f62036aba0c5b2a6fbeb82ff90202
iGabilondo <i.gabilondo2010@gmail.com>
2023-02-01 19:42:00 +0100

    added stars, animations, physics, dudes movements...

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,38 +1,74 @@
-//DEBERES: El juego tiene varias estrellas y varias bombas, no agregar una a una (hacer un for)
-/**
- *! hacer un método para agregar estrellas
- *! hacer un método para agregar bombas
- *! los métodos tendrán un parámetro que indique cuántas bombas/estrellas queremos agregar, y agregará tantas bombas y estrellas como indique el método con posiciones aleatorias.
- */
+//DEBERES: Las bombas deben moverse de por el mapa (aleatoriamente), ponerle velocidadX y velocidadY y hacerle un bounce para que rebote. Tienen que rebotar con los bordes del mundo y con las plataformas.
+
 export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: 'Game' })
+    this.estrellas  = []
   }
 
 
 // El orden es importante al colocar las imágenes. La última creada es la que se ve por encima del resto.
   create() {
     this.cielo = this.add.image(400, 300, "Cielo")
+
+    this.plataformas = this.physics.add.staticGroup()
+    this.plataformas.create(200 ,585 , "Plataforma")
+    this.plataformas.create(600 ,585 , "Plataforma")
+    this.plataformas.create(600 ,500 , "Plataforma")
+    this.plataformas.create(150 ,400 , "Plataforma")
+    this.plataformas.create(600 ,300 , "Plataforma")
+    this.plataformas.create(150 ,190 , "Plataforma")
+    this.plataformas.create(600 ,100 , "Plataforma")
+
+    this.personaje = this.physics.add.sprite(350, 545,"Personaje")
+    this.personaje.setBounce(0.2)
+    this.personaje.setCollideWorldBounds(true)
+    this.physics.add.collider(this.personaje, this.plataformas)
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("Personaje", { start: 0, end: 3 }),
+      frameRate: 15,
+      repeat: -1
+    })
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("Personaje", { start: 5, end: 8 }),
+      frameRate: 15,
+      repeat: -1
+    })
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "Personaje", frame: 4 }],
+      frameRate: 15,
+      repeat: -1
+    })
+
+    this.cursors = this.input.keyboard.createCursorKeys()
+
     this.forParaEstrellas(12)
-    this.forParaBombas(12)
-    this.plataformas = this.add.image(200 ,585 , "Plataforma")
-    this.plataformas = this.add.image(600 ,585 , "Plataforma")
-    this.plataformas = this.add.image(600 ,500 , "Plataforma")
-    this.plataformas = this.add.image(150 ,400 , "Plataforma")
-    this.plataformas = this.add.image(600 ,300 , "Plataforma")
-    this.plataformas = this.add.image(150 ,190 , "Plataforma")
-    this.plataformas = this.add.image(600 ,100 , "Plataforma")
-    this.personaje = this.add.sprite(350, 545,"Personaje")
+    // this.forParaBombas(12)
   }
+
   forParaEstrellas(cuantasEstrellas){
     for (let cuenta = 0; cuenta < cuantasEstrellas; cuenta++){
-    this.estrella = this.add.image((this.getRandomInt(150,610)),(this.getRandomInt(90, 610)),"Estrella")
+      const estrella = this.physics.add.image((this.getRandomInt(30,750)),(this.getRandomInt(30, 500)),"Estrella")
+      estrella.setBounceY(0.5)
+      this.physics.add.collider(estrella, this.plataformas)
+      this.physics.add.overlap(this.personaje, estrella, this.recogerEstrellas)
+      this.estrellas.push(estrella)
+    }
+  }
+
+  recogerEstrellas(personaje, estrella){
+    if (estrella){
+      estrella.disableBody(true, true)
     }
   }
 
   forParaBombas(cuantasBombas){
     for (let cuenta = 0; cuenta < cuantasBombas; cuenta++){
-      this.bomba = this.add.image((this.getRandomInt(150,610)),(this.getRandomInt(90, 610)),"Bomba")
+      this.bomba = this.add.image((this.getRandomInt(30,610)),(this.getRandomInt(30, 750)),"Bomba")
       }
   }
 
@@ -46,5 +82,23 @@ export default class Game extends Phaser.Scene {
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
 
-  update() {}
+  update() {
+    if (this.cursors){
+      if (this.cursors.left.isDown){
+        this.personaje.setVelocityX(-160)
+        this.personaje.anims.play("left", true)
+      }
+      else if (this.cursors.right.isDown){
+        this.personaje.setVelocityX(160)
+        this.personaje.anims.play("right", true)
+      }
+      else{
+        this.personaje.setVelocityX(0)
+        this.personaje.anims.play("turn", true)
+      }
+      if (this.cursors.up.isDown && this.personaje.body.touching.down){
+        this.personaje.setVelocityY(-330)
+      }
+    }
+  }
 }
